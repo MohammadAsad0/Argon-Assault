0cdf1d872f59ebcf546862fa99073abef16b1b22
Mohammad Asad <dustyaxad4@gmail.com>
2020-07-29 20:55:22 +0500

    files

--- Argon Assault/Assets/splashScript.cs
+++ Argon Assault/Assets/splashScript.cs
@@ -1,22 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class splashScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("loadNextLevel", 2f);   
     }
 
-    // Update is called once per frame
-    void Update()
+    void loadNextLevel()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    void OnAwake()
+    void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
